57f41bac2d897d0191fa9732d90ff8860e28a50f
Sasha <tarolv3@gmail.com>
2022-10-04 11:35:31 +0300

    Update Large numbers.py

--- Large numbers.py
+++ Large numbers.py
@@ -1,32 +1,53 @@
-﻿import random;
-import sys;
+﻿import random
+import time
+import decimal
+
+# На жаль не зміг реалізувати генерацію ключів більш ніж на 1024 біти. Буду радий почути якісь поради
 
 def fTask(bit):
-    print("First Task.\n")
-    print("Number of key variants:\n")
+    print("\nFirst Task.\n")
+    print("Number of key variants:")
     variants = {}
     for i in bit:
         variants[i] = 2**i
-        print(f"    for {i} bit:", 2**i, "variants. \n")
+        print(f"    for {i} bit:", 2**i, "variants.")
     return variants
 
 
 def sTask(bit, vars):
-    print("Second Task. \n")
-    print("One hexadecimal takes 4 bits.")
+    print("\nSecond Task. \n")
     keys = []
+    print("Generated keys:")
     for i in bit:
-        key = random.uniform(0, vars[i] - 1)
+        key = hex(int(float(str(decimal.Decimal(random.random()) * (vars[i] - 1)))))
         keys.append(key) 
     print(keys)
+    return keys
     
-64
+def tTask(bit,keys):
+    print("\nThird Task.\n")
+    print("Key brute force time")
+    passwords = []
+    hex = "0123456789abcdef"
+    for i in range(len(keys)):
+        print(f"    For {bit[i]} bit password:")
+        password = ""
+        start = time.time()
+        for k in range(len(keys[i])-2):
+            for letter in hex:         
+                if letter == keys[i][k+2]:
+                    time.sleep(0.00000000000001)
+                    password += keys[i][k+2]
+                    continue
+        end = time.time() - start
+        passwords.append("0x"+password)
+        print(str(end)+" sec")
+    print(f"\nPasswords: {passwords}")
+
 def main():
-    bit = [2**i for i in range (3,13)]
-    # vars = fTask(bit)
-    # sTask(bit, vars)
-    # print(vars)
-    # print(123.1123:.0f)
-    print(sys.maxsize)
+    bit = [2**i for i in range (3,11)] 
+    vars = fTask(bit)
+    keys = sTask(bit, vars)
+    tTask(bit, keys)
 
 main()
