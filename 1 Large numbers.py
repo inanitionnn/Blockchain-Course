e6f0871bf4643c431a5cbe97a4015f677877b56a
Sasha <tarolv3@gmail.com>
2022-11-16 13:25:08 +0200

    1

--- 1 Large numbers.py
+++ 1 Large numbers.py
@@ -4,7 +4,8 @@ import decimal
 
 # На жаль не зміг реалізувати генерацію ключів більш ніж на 1024 біти. Буду радий почути якісь поради
 
-def fTask(bit): #Перше завдання
+
+def fTask(bit):  # Перше завдання
     print("\nFirst Task.\n")
     print("Number of key variants:")
     variants = {}
@@ -14,17 +15,19 @@ def fTask(bit): #Перше завдання
     return variants
 
 
-def sTask(bit, vars): #Друге завдання
+def sTask(bit, vars):  # Друге завдання
     print("\nSecond Task. \n")
     keys = []
     print("Generated keys:")
     for i in bit:
-        key = hex(int(float(str(decimal.Decimal(random.random()) * (vars[i] - 1)))))
-        keys.append(key) 
+        key = hex(
+            int(float(str(decimal.Decimal(random.random()) * (vars[i] - 1)))))
+        keys.append(key)
     print(keys)
     return keys
-    
-def tTask(bit,keys): #Третє завдання
+
+
+def tTask(bit, keys):  # Третє завдання
     print("\nThird Task.\n")
     print("Key brute force time")
     passwords = []
@@ -34,7 +37,7 @@ def tTask(bit,keys): #Третє завдання
         password = ""
         start = time.time()
         for k in range(len(keys[i])-2):
-            for letter in hex:         
+            for letter in hex:
                 if letter == keys[i][k+2]:
                     time.sleep(0.00000000000001)
                     password += keys[i][k+2]
@@ -44,10 +47,12 @@ def tTask(bit,keys): #Третє завдання
         print(str(end)+" sec")
     print(f"\nPasswords: {passwords}")
 
-def main(): 
-    bit = [2**i for i in range (3,11)] 
+
+def main():
+    bit = [2**i for i in range(3, 11)]
     vars = fTask(bit)
     keys = sTask(bit, vars)
     tTask(bit, keys)
+
 
 main()
