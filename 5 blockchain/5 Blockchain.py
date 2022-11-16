1be4b7c8581d1a40f9f0ebd45ec55d32241ea6df
Sasha <tarolv3@gmail.com>
2022-11-16 14:24:38 +0200

    2

--- 5 blockchain/5 Blockchain.py
+++ 5 blockchain/5 Blockchain.py
@@ -1,1 +1,30 @@
-print("hello world")
+class Blockchain():
+    pass
+
+
+class Block():
+    pass
+
+
+class Account():
+    pass
+
+
+class Hash():
+    pass
+
+
+class KeyPair():
+    pass
+
+
+class Signature():
+    pass
+
+
+class Transaction():
+    pass
+
+
+class Operation():
+    pass
